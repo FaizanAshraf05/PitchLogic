33a278cd3dd9ca5b9c02e964f66a5e322c2b9cc3
Faizan Ashraf <faizan.ashraf8122@gmail.com>
2026-04-25 17:10:13 +0500

    Fixed errors

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const config = require('./dbConfig');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 const poolPromise = sql.connect(config)
     .then(pool => {
